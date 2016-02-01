3aa4426cdc6e0e5f5660d0892c4cc90c29285552
nathandud <dudleynathan@byu.net>
2016-02-01 12:27:12 -0700

    Updated models

--- vbahelp/locate/models.py
+++ vbahelp/locate/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,7 +12,21 @@ class ClassroomLayout(models.Model):
     def __str__(self):
         return self.title
 
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class Student(Person):
+    major = models.TextField(max_length=50)
+
+    def __string__(self):
+        return '{} {} ({})'.format(self.user.first_name, self.user.last_name,
+            self.username)
+
 class Ticket(models.Model):
+    student = models.ForeignKey(Student)
     time = models.DateTimeField(auto_now=True)
     student_question = models.TextField(default="")
     completed = models.BooleanField(default=False)
@@ -20,7 +35,7 @@ class Ticket(models.Model):
         return self.id
 
 class StudentLocation(models.Model):
-    ticket = models.ForeignKey(models.ForeignKey(Ticket))
+    ticket = models.ForeignKey(Ticket)
     xcoord = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     ycoord = models.DecimalField(max_digits=10, decimal_places=2, default =0)
     img_width = models.DecimalField(max_digits=10, decimal_places=2, default=0)
@@ -28,3 +43,21 @@ class StudentLocation(models.Model):
 
     def __str__(self):
         return '{}, {}'.format(self.xcoord, self.ycoord)
+
+
+
+'''
+Django-provided fields
+username
+first_name
+last_name
+email
+password
+groups
+user_permissions
+is_staff
+is_active
+is_superuser
+last_login
+date_joined
+'''
